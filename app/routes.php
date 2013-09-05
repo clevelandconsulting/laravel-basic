969d07e0c273a63cb9fc0460cba7c5340ef74d9f
clevelandconsulting <kevinvile@Kevins-iMac.local>
2013-09-05 09:21:18 -0400

    Moved from routes to controllers

--- app/routes.php
+++ app/routes.php
@@ -11,48 +11,13 @@
 |
 */
 
-Route::get('/', array('as'=>'home', function()
-{
+/* Home Route */
+Route::get('/', array('as'=>'home', "uses"=>"HomeController@showMain"));
 
-	return View::make('home');
+/* Routes for logging in and out */
+Route::get('login', array('as'=>'login', "uses"=>"LoginController@showLogin"))->before('guest');
+Route::post('login', "LoginController@doLogin");
+Route::get('logout',array('as'=>'logout',"uses"=>"LoginController@doLogout"))->before('auth');
 
-}));
-
-Route::get('login', array('as'=>'login', function()
-{
-
-	return View::make('login');	
-
-}))->before('guest');
-
-Route::post('login', function()
-{
-	$user = array(
-		'username'=>Input::get('username'),
-		'password'=>Input::get('password')
-	);
-	
-	if ( Auth::attempt($user)) {
-		return Redirect::route('home')->with('flash_notice','You are successfully logged in!');
-	}
-	
-	return Redirect::route('login')->with('flash_error','Your username/password combination was incorrect!')->withInput();
-	
-
-});
-
-Route::get('logout', array('as'=>'logout', function()
-{
-	Auth::logout();
-	
-	return Redirect::route('home')->with('flash_notice','You are successfully logged out.');
-	
-
-}))->before('auth');
-
-Route::get('profile', array('as'=>'profile', function()
-{
-
-	return View::make('profile');	
-
-}))->before('auth');
+/*Routes for the Profile*/
+Route::get('profile', array('as'=>'profile', "uses"=>"ProfileController@showProfile"))->before('auth');
